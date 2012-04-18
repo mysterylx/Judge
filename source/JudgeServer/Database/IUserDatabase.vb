570d0a4f82d0a3d255545eedfa34097d17390839
AssisterForZHG <2310910160@qq.com>
2012-04-18 09:11:31 +0800

    增加接口示例。

---
+++ source/JudgeServer/Database/IUserDatabase.vb
@@ -0,0 +1,9 @@
+﻿Public Interface IUserDatabase
+
+    Property problemNumber As Integer
+
+    Function ClearDatabase() As Boolean
+    Function RandomGenerateUser(ByVal userNumber As Integer) As Boolean
+    Function IsUserValid(ByVal name As String, ByVal password As String) As Boolean
+
+End Interface
