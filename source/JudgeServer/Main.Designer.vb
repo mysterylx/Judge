a284e7aef00289a331ad198097c1b7c4d19a5448
CyberZHG <443442708@qq.com>
2012-04-18 13:51:46 +0800

    开始写网络方面的界面。
    Signed-off-by: CyberZHG <443442708@qq.com>

--- source/JudgeServer/Main.Designer.vb
+++ source/JudgeServer/Main.Designer.vb
@@ -22,8 +22,56 @@ Partial Class Main
     '不要使用代码编辑器修改它。
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.ServerTab = New System.Windows.Forms.TabControl()
+        Me.ProblemTabPage = New System.Windows.Forms.TabPage()
+        Me.TabPage2 = New System.Windows.Forms.TabPage()
+        Me.ServerTab.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'ServerTab
+        '
+        Me.ServerTab.Controls.Add(Me.ProblemTabPage)
+        Me.ServerTab.Controls.Add(Me.TabPage2)
+        Me.ServerTab.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.ServerTab.Location = New System.Drawing.Point(0, 0)
+        Me.ServerTab.Name = "ServerTab"
+        Me.ServerTab.SelectedIndex = 0
+        Me.ServerTab.Size = New System.Drawing.Size(484, 447)
+        Me.ServerTab.TabIndex = 0
+        '
+        'ProblemTabPage
+        '
+        Me.ProblemTabPage.Location = New System.Drawing.Point(4, 22)
+        Me.ProblemTabPage.Name = "ProblemTabPage"
+        Me.ProblemTabPage.Padding = New System.Windows.Forms.Padding(3)
+        Me.ProblemTabPage.Size = New System.Drawing.Size(476, 421)
+        Me.ProblemTabPage.TabIndex = 0
+        Me.ProblemTabPage.Text = "Problem Setting"
+        Me.ProblemTabPage.UseVisualStyleBackColor = True
+        '
+        'TabPage2
+        '
+        Me.TabPage2.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage2.Name = "TabPage2"
+        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage2.Size = New System.Drawing.Size(476, 421)
+        Me.TabPage2.TabIndex = 1
+        Me.TabPage2.Text = "Network Setting"
+        Me.TabPage2.UseVisualStyleBackColor = True
+        '
+        'Main
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "Main"
+        Me.ClientSize = New System.Drawing.Size(484, 447)
+        Me.Controls.Add(Me.ServerTab)
+        Me.Name = "Main"
+        Me.Text = "Judge Server"
+        Me.ServerTab.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
     End Sub
+    Friend WithEvents ServerTab As System.Windows.Forms.TabControl
+    Friend WithEvents ProblemTabPage As System.Windows.Forms.TabPage
+    Friend WithEvents TabPage2 As System.Windows.Forms.TabPage
 End Class
--- source/JudgeServer/Main.vb
+++ source/JudgeServer/Main.vb
@@ -1,3 +1,10 @@
 ﻿Public Class Main
+    Private ProblemControler As ProblemControl
+
+    Private Sub Main_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        ProblemControler = New ProblemControl
+        ProblemControler.Dock = DockStyle.Fill
+        Me.ProblemTabPage.Controls.Add(ProblemControler)
+    End Sub
 
 End Class
