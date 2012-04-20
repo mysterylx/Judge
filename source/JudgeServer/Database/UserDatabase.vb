2b6c22008ffda1e4071fdceb0e67796f27e27ef2
unknown <whjlx@whjlx-PC.(none)>
2012-04-20 19:40:38 +0800

    add funciton change password

--- source/JudgeServer/Database/UserDatabase.vb
+++ source/JudgeServer/Database/UserDatabase.vb
@@ -37,8 +37,10 @@
 
     Function RandomGenerateUser(ByVal userNumber As Integer) As Boolean
         Dim rand = New System.Random()
-        Dim id As Integer
-        Dim passwd As Integer
+        Dim i As Integer
+        Dim randnum As Integer
+        Dim name As String
+        Dim passwd As String
         Dim used(1000) As Boolean
         Dim connString As String
         Dim connection As SqlConnection
@@ -46,17 +48,19 @@
         connection = New SqlConnection(connString)
         Try
             connection.Open()
-            For id = 1 To userNumber Step 1
-                While passwd = rand.next(1000)
-                    If used(passwd) <> True Then
-                        used(passwd) = True
+            For i = 1 To VProblemNumber Step 1
+                While randnum = rand.next(1000, 9999)
+                    If used(randnum) <> True Then
+                        used(randnum) = True
                         Exit While
                     Else
                         Continue While
                     End If
                 End While
                 Try
-                    Dim sqlstring As String = "insert into users (username,userpassword) values ('" & id & "','" & passwd & "')"
+                    name = "Team" & Str(i)
+                    passwd = Str(randnum)
+                    Dim sqlstring As String = "insert into users (username,userpassword) values ('" & name & "','" & passwd & "')"
                     Dim cmd As New SqlCommand(sqlstring, connection)
                     Try
                         cmd.ExecuteNonQuery()
@@ -80,18 +84,16 @@
     End Function
 
     Function IsUserValid(ByVal name As String, ByVal password As String) As Boolean
+        Dim Valid As Boolean
         Dim id As Integer
         Dim passwd As Integer
         Dim reader As SqlDataReader
-        Dim Valid As Boolean
-        id = Val(name)
-        passwd = Val(password)
         Dim connection As SqlConnection
         connString = "Data Source = localhost;Initial Catalog = MatchInformation;integrated security=true"
         connection = New SqlConnection(connString)
         Try
             connection.Open()
-            Dim sqlstring As String = "select * from users where username = '" & id & "'And userpassword ='" & passwd & "'"
+            Dim sqlstring As String = "select * from users where username = '" & name & "'And userpassword ='" & password & "'"
             Dim command As New SqlCommand(sqlstring, connection)
             reader = command.ExecuteReader
             If reader.Read() = True Then
@@ -110,4 +112,29 @@
         Return Valid
     End Function
 
+    Function ChangePassword(ByVal name As String, ByVal password As String) As Boolean
+        Dim Change As Boolean
+        Dim connString As String
+        connString = "Data Source = localhost;Initial Catalog = MatchInformation;integrated security=true"
+        Dim connection = New SqlConnection(connString)
+        Try
+            connection.Open()
+            Dim sqlsearch As String = "select * from users where username = '" & name & "' "
+            Dim sqlupdate As String = "update users set userpassword = '" & passwd & " ' where username = ' " & name & " ' "
+            Dim cmdsearch As New SqlCommand(sqlsearch, connection)
+            Dim cmdupdate As New SqlCommand(sqlupdate, connection)
+            Dim reader As SqlDataReader = cmdsearch.ExecuteReader
+            If reader.Read() = True Then
+                reader.Close()
+                cmdupdate.ExecuteNonQuery()
+            End If
+        Catch OleDbExceptionErr As OleDbException
+            Debug.WriteLine(OleDbExceptionErr.Message)
+        Catch InvalidOperationExceptionErr As InvalidOperationException
+            Debug.WriteLine(InvalidOperationExceptionErr.Message)
+        Catch sqlExceptionErr As SqlException
+            Debug.WriteLine(sqlExceptionErr.Message)
+        End Try
+        Return Change
+    End Function
 End Class
